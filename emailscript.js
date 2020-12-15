6cbe0b2a9e90cdc154e72b43558a61068d96d7e0
SamuOjala <samu.ojala@edu.salpaus.fi>
2020-12-15 16:14:08 +0000

    20201215 18:14

--- emailscript.js
+++ emailscript.js
@@ -7,40 +7,57 @@ const virheteksti = document.querySelector('#virheteksti');
 emailnappi.addEventListener('click', e => {
   e.preventDefault();
   sendJSON();
-  virhetapahtuma();
 });
 
 function sendJSON(){
-  let xhr = new XMLHttpRequest();
-  let url = "https://salpausemail.azurewebsites.net/api/HttpTriggerCSharp2?code=PnWhScmEcspN8Fy7eYKnIZA37AFgUZ0fMQ1OpXOJ6dtBPBGNXAMIqQ==";
+  if(viesti.value ===''|| email.value ===''||nimi.value ===''){
+    virhe.style.background ='#ff0000';
+    virhe.style.border ='solid 2px black';
+    virheteksti.innerHTML=('!!!!     Täytä kaikki kentät, Kiitos.     !!!!')
+    setTimeout(()=> virhe.style.background = 'rgb(160, 190, 190)',3000);
+    setTimeout(()=> virhe.style.border = '0px',3000);
+    setTimeout(()=> virhe.innerHTML = '', 3000);
+  }else{
+    let xhr = new XMLHttpRequest();
+    let url = "https://salpausemail.azurewebsites.net/api/HttpTriggerCSharp2?code=PnWhScmEcspN8Fy7eYKnIZA37AFgUZ0fMQ1OpXOJ6dtBPBGNXAMIqQ==";
 
-  xhr.open("POST", url, true);
+    xhr.open("POST", url, true);
 
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.onreadystatechange = function (){
-    if(xhr.readyState === 4 && xhr.status === 200){
-    console.log("Valmis, yhteys toimii.");
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function(){
+      if(xhr.readyState === 4 && xhr.status === 200){
+        console.log("valmis, yhteys toimii");
+      }
+    };
+    const nimi = document.querySelector('#nimi').value;
+    const email = document.querySelector('#email').value;
+    console.log("nimikentän sisältö: " + nimi);
+    const viesti = document.querySelector('#viesti').value;
+    console.log("viestikentän sisältö: " + viesti);
+    var data = JSON.stringify({
+      "EmailMsg": viesti,  //Kirjoittaa sisällön 
+      "EmailTo": "samu.ojala", //oma sähköpostisi!!!!
+      "EmailName": nimi//Nimi-kentän sisältö
+    });
+    xhr.send(data);
   }
-};
-var data = JSON.stringify({
-  "EmailMsg": viesti, /*kirjoittaa sisällön*/
-  "EmailAddress": email, /*  Viestin kirjoittajan osoite*/
-  "EmailTo": "Samu.Ojala", /*Sivunpitäjän osoite*/
-  "EmailName": nimi +" "+ email /*Nimi-kentän sisältö*/
-});
+}
 
-};
 
 function virhetapahtuma(){
 if(viesti.value ===''|| email.value ===''||nimi.value ===''){
-virhe.style.background='#ff0000';
-virhe.style.border= 'solid 2px black';
+virhe.style.background ='#ff0000';
+virhe.style.border ='solid 2px black';
 virheteksti.innerHTML=('!!!!     Täytä kaikki kentät, Kiitos.     !!!!')
-setTimeOut(function(){virhe.style.background = 'white'},555550);
+setTimeout(()=> virhe.style.background = 'rgb(160, 190, 190)',3000);
+setTimeout(()=> virhe.style.border = '0px',3000);
+setTimeout(()=> virhe.innerHTML = '', 3000);
 }else{
   xhr.send(data);
+  sendJSON();
   emailnappi.disabled = true;
-  setTimeOut(function(){emailnappi.disabled = false;},55555000);
+  setTimeout(function(){emailnappi.disabled = false;},55555000);
   virheteksti.innerHTML = 'Kiitos!';
 }
 };
--- javascript.js
+++ javascript.js
@@ -1,100 +1,63 @@
+if(document.querySelector('#AsetusLogo') != null){
+  const asetuksetLogo = document.querySelector('#AsetusLogo');
+  const asetukset = document.querySelector('.Asetukset');
+  const asetusOtsikko = document.querySelector('#asetusotsikko');
 
-function iat(ekaNumero, tokaNumero, kolmasNumero, neljasNumero, viidesNumero){
-  return(ekaNumero + tokaNumero + kolmasNumero + neljasNumero + viidesNumero)
-}
-const nimi = 'Samu';
-const ika = 16;
-const opiskelija = true;
-
-console.log(`Hei surffailija, olen ${nimi} ja olen ${ika} vuotias.`)
-console.log('Opiskelija? '+opiskelija)
-
-
-const ystavani = ['Wubbi Swedu', 'Jesse Pulmu', 'Nacho Saurusz', 'Minä Siepari'];
-console.log('1. Ystäväni '+ystavani[0].split(' ')[0]);
-console.log('2. Ystäväni '+ystavani[1].split(' ')[0]);
-console.log('3. Ystäväni '+ystavani[2].split(' ')[0]);
-console.log('4. Ystäväni '+ystavani[3].split(' ')[0]);
-const perhe= [
-  {
-    id: 1,
-    nimi: 'Isä',
-    ika: 40,
-    taysiikainen:true
-  },
-  {
-    id: 2,
-    nimi: 'Äiti',
-    ika: 40,
-    taysiikainen:true
-  },
-  {
-    id: 3,
-    nimi: 'Sisko yksi',
-    ika: 14,
-    taysiikainen:false
-  },
-  {
-    id: 4,
-    nimi: 'Sisko kaksi',
-    ika: 11,
-    taysiikainen:false
-  }
-];
-
-
-const perheTaysi = perhe.filter(function(perhe){
-  return perhe.taysiikainen === true;
-}).map(function(perhe){
-  console.log(perhe.nimi+' on täysi-ikäinen');
-});
-const perheAla = perhe.filter(function(perhe){
-  return perhe.taysiikainen === false;
-}).map(function(perhe){
-  console.log(perhe.nimi+' on alaikäinen');
-});
-
-console.log('Perheeni jäsenten ikien summa on: '+ iat(11, 14, 16, 40, 40 ));
-
-const asetuksetLogo = document.querySelector('#AsetusLogo');
-const asetukset = document.querySelector('.Asetukset');
-const asetusOtsikko = document.querySelector('#asetusotsikko');
-
-asetuksetLogo.addEventListener('click', e =>{
-  asetuksetTapahtuma();
-});
-function asetuksetTapahtuma() {
-  var x = asetukset;
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-};
-
-const etuSisalto = document.querySelector('#etu');
-const sukuSisalto = document.querySelector('#suku');
-const sahkoSisalto = document.querySelector('#sahko');
-const virheilmo = document.querySelector('#virhetila');
-const nappiYhteys = document.querySelector('.yhteysnappi');
-
-nappiYhteys.addEventListener('click', e =>{
-  e.preventDefault();
-  if(etuSisalto.value ==='' || sukuSisalto.value ==='' || sahkoSisalto.value ==='' ){
-    virheilmo.classList.add('virheilmo')
-    virheilmo.innerHTML = 'Täytä kentät pakolliset kentät!';
-    nappula.style.background = 'red';
-    nappula.value= 'Lisää tieto!';
-    setTimeout(()=> virheilmo.classList.remove('virheilmo'), 3000);
-    setTimeout(()=> nappiyhteys.style.background = '#b45b07', 3000);
-    setTimeout(()=> nappiyhteys.value='Lähetä.', 3000);
-  }else{
-
-    const li = document.createElement('li');
+  asetuksetLogo.addEventListener('click', e =>{
+    e.preventDefault();
     
-
-    etuSisalto.value = '';
-    sukuSisalto.value= '';
-    sahkoSisalto.value='';
+/*
+ const teema = document.querySelector('input[name="theme"]:checked').value;
+  if(teema == 'tumma'){
+    document.body.style.background = 'black';
   }
-});
+  else{
+    document.body.style.background = 'white';
+  }*/
+
+  const teema2 = document.querySelector('input[name="theme"]:checked').id;
+    if(teema2== 'tumma'){
+      document.body.style.background = 'black';
+      document.body.style.color = 'white';
+    }else if(teema2 == 'vaalea'){
+      document.body.style.background = 'white';
+      document.body.style.color = 'black';
+    }
+    else{
+      document.body.style.background = '#6d8bcc';
+      document.body.style.color = 'black';
+    }
+    
+  });
+}
+
+
+if(document.querySelector('.yhteysnappi') != null)
+{
+  const etuSisalto = document.querySelector('#etu');
+  const sukuSisalto = document.querySelector('#suku');
+  const sahkoSisalto = document.querySelector('#sahko');
+  const virheilmo = document.querySelector('#virhetila');
+  const nappiYhteys = document.querySelector('.yhteysnappi');
+
+  nappiYhteys.addEventListener('click', e => {
+    e.preventDefault();
+    if(etuSisalto.value ==='' || sukuSisalto.value ==='' || sahkoSisalto.value ==='' ){
+      virheilmo.classList.add('virheilmo')
+      virheilmo.innerHTML = 'Täytä kentät pakolliset kentät!';
+      nappula.style.background = 'red';
+      nappula.value= 'Lisää tieto!';
+      setTimeout(()=> virheilmo.classList.remove('virheilmo'), 3000);
+      setTimeout(()=> nappiyhteys.style.background = '#b45b07', 3000);
+      setTimeout(()=> nappiyhteys.value='Lähetä.', 3000);
+    }else{
+
+      const li = document.createElement('li');
+      
+
+      etuSisalto.value = '';
+      sukuSisalto.value= '';
+      sahkoSisalto.value='';
+    }
+  });
+}
